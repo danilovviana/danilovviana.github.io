6e3e3cdff58a2b798b14fdf484ac6bb13256919f
Danilo Viana <danilovviana@gmail.com>
2023-10-20 11:53:02 -0300

    attt

---
+++ img/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+    return ( <
+        div >
+        <
+        > 3 <
+        h1 > Teste < /h1> <
+        /> <
+        /div>
+    )
+}
+
+export default App
